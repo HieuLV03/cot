4a029ae155637d40a74ef7ca079dc1a2c838f1e5
HieuLV03 <levanhieu14102k3@gmail.com>
2026-07-13 18:18:27 +0700

    g

--- app/(auth)/callback/route.ts
+++ app/(auth)/callback/route.ts
@@ -13,17 +13,7 @@ export async function GET(request: Request) {
 
     const supabase = await createClient();
 
-
-    const {
-      error
-    } = await supabase.auth.exchangeCodeForSession(
-      code
-    );
-
-
-    if(error){
-      console.log(error);
-    }
+    await supabase.auth.exchangeCodeForSession(code);
 
   }
 
