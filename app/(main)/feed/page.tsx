f967f09c29990f0924d900cb74292e3e11ea2d1e
HieuLV03 <levanhieu14102k3@gmail.com>
2026-07-13 20:03:06 +0700

    f

--- app/(main)/feed/page.tsx
+++ app/(main)/feed/page.tsx
@@ -6,78 +6,137 @@ export default async function FeedPage() {
 
   const supabase = await createClient();
 
+
   const { data: photos, error } = await supabase
     .from("photos")
-    .select("*")
+    .select(`
+      id,
+      image_url,
+      created_at,
+      profiles (
+        username,
+        full_name,
+        avatar_url
+      )
+    `)
     .order("created_at", {
       ascending: false,
     });
 
+
   if (error) {
+
     return (
       <section className="p-4">
         <p>{error.message}</p>
       </section>
     );
+
   }
 
+
   return (
+
     <section className="space-y-4 p-4">
 
-      {photos?.map((photo) => (
+
+      {photos?.map((photo:any) => (
 
         <div
           key={photo.id}
-          className="overflow-hidden rounded-3xl bg-white shadow-sm"
+          className="
+            overflow-hidden
+            rounded-3xl
+            bg-white
+            shadow-sm
+          "
         >
+
 
           <div className="relative aspect-square">
 
             <Image
+
               src={photo.image_url}
+
               alt=""
+
               fill
+
               unoptimized
+
               className="object-cover"
+
             />
 
           </div>
 
+
+
           <div className="p-4">
 
+
             <p className="font-semibold">
-              {photo.user_name}
+
+              {
+                photo.profiles?.full_name ||
+                photo.profiles?.username ||
+                "Người dùng"
+              }
+
             </p>
+
 
             <p className="text-sm text-neutral-500">
-              {new Date(
-                photo.created_at
-              ).toLocaleString()}
+
+              {
+                new Date(
+                  photo.created_at
+                ).toLocaleString("vi-VN")
+              }
+
             </p>
 
+
           </div>
+
 
         </div>
 
       ))}
 
+
+
       {photos?.length === 0 && (
 
-        <div className="rounded-3xl bg-white p-6 shadow-sm">
+        <div className="
+          rounded-3xl
+          bg-white
+          p-6
+          shadow-sm
+        ">
 
           <h2 className="text-lg font-semibold">
             Welcome 👋
           </h2>
 
-          <p className="mt-2 text-sm text-neutral-500">
+
+          <p className="
+            mt-2
+            text-sm
+            text-neutral-500
+          ">
             Chưa có bài viết nào.
           </p>
+
 
         </div>
 
       )}
 
+
     </section>
+
   );
 
 }
